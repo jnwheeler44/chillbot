c96cc6e00dd587714fb8f364512191c7cc3d4595
Matt Culpepper <matt@culpepper.co>
2013-06-24 20:16:26 -0500

    fix these hardcoded channel messages

--- controllers/bitcoin_controller.rb
+++ controllers/bitcoin_controller.rb
@@ -12,16 +12,16 @@ class BitcoinController < Rubot::Controller
   def do_shit_aight
     response = Bitcoin::Call.new
 
-    server.message "#iz", "[bitcoin] 1 BTC = #{response.average_display} -- last price: #{response.last_display}"
+    reply "[bitcoin] 1 BTC = #{response.average_display} -- last price: #{response.last_display}"
 
     if response.really_good_time_to_buy?
-      server.message "#iz", "hey mculp, it's a *really* good time to buy."
+      reply "hey mculp, it's a *really* good time to buy."
     elsif response.good_time_to_buy?
-      server.message "#iz", "hey mculp, it's a good time to buy."
+      reply "hey mculp, it's a good time to buy."
     elsif response.SELL_QUICK_WTF_COME_ON?
-      server.message "#iz", "hey mculp, it's a good time to sell."
+      reply "hey mculp, it's a good time to sell."
     elsif response.HOLY_FUCKING_SHIT_SELL_NOW?
-      server.message "#iz", "hey mculp, it's a *really* good time to sell."
+      reply "hey mculp, it's a *really* good time to sell."
     end
   end
 end
