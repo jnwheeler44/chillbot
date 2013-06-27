0e0bc016395ebd3565a0179d35b97d9a1599b1b9
Michael Raeder <radar@Michaels-MacBook-Pro.local>
2013-06-27 13:52:31 -0500

    k

--- resources/meme_generator.rb
+++ resources/meme_generator.rb
@@ -1,52 +1,24 @@
-# encoding: UTF-8
+require 'json'
 
-class EmoticonController < Rubot::Controller
-  command :lod do
-    reply "ಠ_ಠ"
+class MemeGenerator < Rubot::WebResource
+  CONFIG = YAML.load_file "meme_generator.yml"
+  get :generate, 'http://version1.api.memegenerator.net/Instance_Create' do |doc|
+    response = JSON.parse doc
+
+    if response['success']
+      response['result']['instanceImageUrl']
+    else
+      response['errorMessage']
+    end
   end
 
-  command :shrug do
-    reply "¯\\_(ツ)_/¯"
+  def self.create generator_id, image_id, text0, text1
+    self.generate :username => CONFIG['username'], :password => CONFIG['password'],
+      :languageCode => 'en', :generatorID => generator_id, :imageID => image_id,
+      :text0 => text0, :text1 => text1
   end
 
-  command :fliptable do
-    reply "(╯°□°）╯︵ ┻━┻"
+  def self.good_news text
+    self.create '60382', '1430917', 'GOOD NEWS EVERYONE', text
   end
-
-  command :zoidberg do
-    reply "(\\/) (°,,°) (\\/)"
-  end
-  command :yuno do
-    reply "(ノಠ益ಠ)ノ"
-  end
-
-  command :facepalm do
-    reply "(>ლ)"
-  end
-
-  command :flipall do
-    reply "┻━┻︵ \(°□°)/ ︵ ┻━┻"
-  end
-
-  command :zerofucks do
-    reply "╭∩╮（︶︿︶）╭∩╮"
-  end
-
-  command :kill do
-    reply "(╯°□°)--︻╦╤─ - - -"
-  end
-
-  command :huh do 
-    reply "•͡˘㇁•͡˘"
-  end
-
-  command :omg do 
-    reply "⨀_⨀"
-  end
-
-  command :bewbs do 
-    reply "(. )( .)"
-  end
-
-
 end
