4f84794f4367bbe6b8f8246c26f4c2582649c398
Matt Culpepper <matt@culpepper.co>
2013-12-14 20:41:45 -0600

    test3

--- resources/dogecoin.rb
+++ resources/dogecoin.rb
@@ -2,8 +2,10 @@ require 'yajl'
 require 'open-uri'
 
 class Dogecoin < Rubot::WebResource
-  get :price, "https://coinedup.com/OrderBook?market=DOGE&base=BTC" do |doc|
-    doc.css('a[href="OrderBook?market=DOGE&base=BTC"] div')[0].text.split.last[0..-2]
+  def self.price
+    lol = open('https://coinedup.com/OrderBook?market=DOGE&base=BTC').read
+    lol = lol.split(/DOGE\/BTC<div style='font-size:0.85em;text-align:center'>\(last:/).last
+    lol.split(")</div>").first.strip
   end
 
   def self.price_of(number)
