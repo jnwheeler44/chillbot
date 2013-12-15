d05a7c88bc3d4863975b1fb1d2ed1109f95934d7
Matt Culpepper <matt@culpepper.co>
2013-12-14 20:46:12 -0600

    test5

--- resources/dogecoin.rb
+++ resources/dogecoin.rb
@@ -1,9 +1,10 @@
 require 'yajl'
 require 'open-uri'
+require 'openssl'
 
 class Dogecoin < Rubot::WebResource
   def self.price
-    lol = open('http://coinedup.com/OrderBook?market=DOGE&base=BTC').read
+    lol = open('https://coinedup.com/OrderBook?market=DOGE&base=BTC', ssl_verify_mode: OpenSSL::SSL::VERIFY_NONE).read
     lol = lol.split(/DOGE\/BTC<div style='font-size:0.85em;text-align:center'>\(last:/).last
     lol.split(")</div>").first.strip
   end
