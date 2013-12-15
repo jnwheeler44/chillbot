94258fd061ea551baf18809780847e2ccea9bdb5
Matt Culpepper <matt@culpepper.co>
2013-12-14 20:43:16 -0600

    test4

--- resources/dogecoin.rb
+++ resources/dogecoin.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 class Dogecoin < Rubot::WebResource
   def self.price
-    lol = open('https://coinedup.com/OrderBook?market=DOGE&base=BTC').read
+    lol = open('http://coinedup.com/OrderBook?market=DOGE&base=BTC').read
     lol = lol.split(/DOGE\/BTC<div style='font-size:0.85em;text-align:center'>\(last:/).last
     lol.split(")</div>").first.strip
   end
