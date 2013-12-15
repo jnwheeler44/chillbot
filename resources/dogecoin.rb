50458fee94306be8160567efaef8bf43f539c78c
Matt Culpepper <matt@culpepper.co>
2013-12-14 20:29:19 -0600

    dafu

--- resources/dogecoin.rb
+++ resources/dogecoin.rb
@@ -3,6 +3,7 @@ require 'open-uri'
 
 class Dogecoin < Rubot::WebResource
   get :price, "https://coinedup.com/OrderBook?market=DOGE&base=BTC" do |doc|
+    puts doc.css('#elementDisplayLastPrice div')
     doc.css('#elementDisplayLastPrice div')[0].text.split.last[0..-2]
   end
 
