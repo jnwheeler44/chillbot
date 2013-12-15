e97d20f971664beba06eedd2831edb5124220f83
Matt Culpepper <matt@culpepper.co>
2013-12-14 20:35:07 -0600

    test2

--- resources/dogecoin.rb
+++ resources/dogecoin.rb
@@ -3,7 +3,7 @@ require 'open-uri'
 
 class Dogecoin < Rubot::WebResource
   get :price, "https://coinedup.com/OrderBook?market=DOGE&base=BTC" do |doc|
-    doc.css('#elementDisplayLastPrice div')
+    doc.css('a[href="OrderBook?market=DOGE&base=BTC"] div')[0].text.split.last[0..-2]
   end
 
   def self.price_of(number)
