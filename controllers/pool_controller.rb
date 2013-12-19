957780764b0dc7a54db009c38210c33322e8d08a
Matt Culpepper <matt@culpepper.co>
2013-12-19 12:11:36 -0600

    pool schedule stuff

---
+++ controllers/pool_controller.rb
@@ -0,0 +1,19 @@
+class PoolController < Rubot::Controller
+  SUBSCRIBED_CHANNELS = ["#sfs", "#chunky"]
+
+  on :connect do
+    Scheduler.every "1m" do
+      check_for_new_block
+    end
+  end
+
+  def check_for_new_block
+    if block = Pool.check_for_new_block
+      msg = "[pool notice] BLOCK FOUND. REWARD: #{block[:reward]}. FOUND BY: #{block[:found_by]}. SHARES: #{block[:shares]}"
+
+      SUBSCRIBED_CHANNELS.each do |channel|
+        server.msg channel, msg
+      end
+    end
+  end
+end
---
+++ resources/pool.rb
@@ -0,0 +1,36 @@
+require 'open-uri'
+
+module Pool
+  API_KEY = "6428cc89812e76dc925e4d6f3f27cffe8cdf33e5b4509ab29377fbe2aaff5399"
+  URL = "http://pool.chunky.ms/index.php?page=api&api_key=#{API_KEY}"
+
+  def self.check_for_new_block
+    response = grab_and_parse :getpoolstatus
+    current_block_number = response['lastblock']
+
+    block_found = @last_block_number != current_block_number
+
+    @last_block_number = current_block_number
+
+    block_info if block_found
+  end
+
+
+  private
+
+  def self.grab_and_parse(action)
+    JSON.parse(open(action(action)).read)[action.to_s]['data']
+  end
+
+  def self.block_info
+    response = grab_and_parse :getblocksfound
+
+    block = response.find { |block| block.height == @last_block_number }
+
+    { reward: block['amount'].to_i, found_by: block['worker_name'], shares: "#{block['shares']} / #{block['estshares']}" }
+  end
+
+  def self.action(action)
+    URL + "&action=#{action}"
+  end
+end
