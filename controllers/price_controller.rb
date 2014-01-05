8fd59a74d967698a542e319bb72b543cdab5f2ae
Matt Culpepper <matt@culpepper.co>
2014-01-05 15:39:01 -0600

    price

---
+++ controllers/price_controller.rb
@@ -0,0 +1,28 @@
+class PriceController < Rubot::Controller
+  command :price, :worth do
+    args = message.text.split
+
+    if args.size == 2
+      do_shit_with_numbers(args[0], args[1])
+    elsif args.size == 1
+      do_shit_aight(args[0])
+    elsif args.first.empty?
+      reply "usage: !price [coin] [amount (optional)]"
+    end
+  end
+
+  def do_shit_aight(coin)
+    price = Price.price(coin)
+    reply "[#{coin.upcase} price] 1 #{coin.upcase} = #{price} BTC | 1 MEGA#{coin.upcase} = #{1000000 * price.to_f} BTC"
+  end
+
+  def do_shit_with_numbers(coin, number)
+    number_btc = Earthcoin.price_of(coin, number)
+
+    btc_response = Bitcoin::Call.new
+    number_usd = btc_response.last_worth(number_btc)
+
+    reply "[#{coin.upcase} price] #{number} #{coin.upcase} = #{number_btc} BTC | #{number} #{coin.upcase} = $#{number_usd} USD"
+  end
+end
+
---
+++ resources/price.rb
@@ -0,0 +1,15 @@
+require 'yajl'
+require 'open-uri'
+require 'openssl'
+
+class Price
+  URL = 'http://www.cryptocoincharts.info/v2/api/listCoins'
+
+  def self.price(coin)
+    JSON.parse(open(URL).read).find { |item| item['id'] == coin.downcase }['price_btc']
+  end
+
+  def self.price_of(coin, number)
+    number.to_f * price(coin).to_f
+  end
+end
