aea470b68ef2b71b4f18e9d6f1da1dbbb1cd5627
Matt Culpepper <matt@culpepper.co>
2014-01-05 19:56:23 -0600

    khash calc2

--- resources/hash.rb
+++ resources/hash.rb
@@ -5,7 +5,7 @@ require 'openssl'
 class Hash
   COINWARZ_URL = 'http://www.coinwarz.com/v1/api/coininformation/?apikey=e25ed31c94e1459aa1088195cccbae8f&cointag='
 
-  def self.difficulty(coin)
+  def self.difficulty_and_reward(coin)
     case coin.upcase
     when 'DOGE'
       { difficulty: Pool.new(:doge).pool_status[:difficulty], reward: 500000 }
