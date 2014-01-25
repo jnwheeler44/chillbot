7c1c7c0357783b56e392cc63f5062942d506d62e
Matt Culpepper <matt@culpepper.co>
2014-01-24 23:41:32 -0600

    derp

--- resources/price.rb
+++ resources/price.rb
@@ -8,9 +8,13 @@ class Price
   URL = 'http://www.cryptocoincharts.info/v2/api/listCoins'
 
   def self.price(coin)
-    if coin.to_s.downcase == 'dgb'
-      body = Typhoeus.get('https://www.coinmarket.io', ssl_verifypeer: false, nosignal: true).response_body
-      Nokogiri::HTML.parse(body).css('.ticker-DGBBTC')[0].text
+    if coin.to_s.downcase == 'dgb' || coin.to_s.downcase == 'kdc'
+      begin
+        body = Typhoeus.get('https://www.coinmarket.io', ssl_verifypeer: false, nosignal: true).response_body
+        Nokogiri::HTML.parse(body).css(".ticker-#{coin.to_s.upcase}BTC")[0].text
+      rescue
+        coin.to_s.downcase == 'dgb' ? 0.0000025 : 0.00024
+      end
     else
       body = Typhoeus.get(URL, nosignal: true).response_body
       JSON.parse(body).find { |item| item['id'] == coin.to_s.downcase }['price_btc']
