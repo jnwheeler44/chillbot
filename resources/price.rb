d6e03b65c5f866d22d07d4172fd69c2f58828a55
Matt Culpepper <matt@culpepper.co>
2014-02-03 11:25:29 -0600

    updatz

--- resources/price.rb
+++ resources/price.rb
@@ -17,7 +17,15 @@ class Price
         0
       end
     elsif coin.downcase == 'pot'
-      0.000033
+      begin
+        body = Typhoeus.get('https://freshmarket.co.in/index.php?page=trade&market=124', ssl_verifypeer: false, nosignal: true).response_body
+        ltc_price = Nokogiri::HTML.parse(body).css("#user-orders tr:nth-child(2) > td:nth-child(1)")[0].text
+
+        ltc_price.to_f * price('ltc').to_f
+      rescue
+        puts "error scraping coinmarket"
+        0
+      end
     else
       body = Typhoeus.get(URL, nosignal: true).response_body
       JSON.parse(body).find { |item| item['id'] == coin.to_s.downcase }['price_btc']
